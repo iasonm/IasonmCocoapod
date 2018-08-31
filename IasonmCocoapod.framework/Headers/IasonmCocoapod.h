a333198b5209f864e7bfeac66d293f09d746d4c7
Iason Michailidis <iasonm57@gmail.com>
2018-08-31 13:56:03 +0300

    Publish framework

---
+++ IasonmCocoapod.framework/Headers/IasonmCocoapod.h
@@ -0,0 +1,19 @@
+//
+//  IasonmCocoapod.h
+//  IasonmCocoapod
+//
+//  Created by Iason Michailidis on 31/08/2018.
+//  Copyright © 2018 Iason Michailidis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IasonmCocoapod.
+FOUNDATION_EXPORT double IasonmCocoapodVersionNumber;
+
+//! Project version string for IasonmCocoapod.
+FOUNDATION_EXPORT const unsigned char IasonmCocoapodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IasonmCocoapod/PublicHeader.h>
+
+
---
+++ IasonmCocoapod/IasonmCocoapod.swift
@@ -0,0 +1,15 @@
+//
+//  IasonmCocoapod.swift
+//  IasonmCocoapod
+//
+//  Created by Iason Michailidis on 31/08/2018.
+//  Copyright © 2018 Iason Michailidis. All rights reserved.
+//
+
+import Foundation
+
+public class IasonmCocoapod {
+    public class func logToConsole(msg: String) {
+        print(msg);
+    }
+}
